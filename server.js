3d3cdea1d12f54b611e6edfa2799e9faafae00f2
ppaint27 <84406560+ppaint27@users.noreply.github.com>
2022-10-29 19:48:25 +0700

    connectDB

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const cors = require('cors')
+
+app.use(express.json());
+app.use(cors())
+
+app.use("/api/v2", require("./src/middleware/api"));
+app.listen(8081, () => console.log("server is running"));
---
+++ src/middleware/api.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+require("../db");
+
+//public zone
+const { verify } = require("../jwt");
+
+// scured veify zone
+
+
+
+
+module.exports = router;
