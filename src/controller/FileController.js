e3ccaa4bac3eed7b036e4fe1379f4c7dcfd8f856
ppaint27 <84406560+ppaint27@users.noreply.github.com>
2022-12-03 17:25:17 +0700

    upload file

--- src/controller/FileController.js
+++ src/controller/FileController.js
@@ -1,6 +1,10 @@
 const multer = require('multer');
 const Files = require('../models/file_schema');
 
+
+const currentTime = Date.now();
+
+
 const storageImg = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, 'public/img/')
@@ -9,10 +13,19 @@ const storageImg = multer.diskStorage({
         cb(null, currentTime + '-' + file.originalname)
     }
 });
+
+const storagePdf = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/pdf/')
+    },
+    filename: (req, file, cb) => {
+        cb(null, currentTime + '-' + file.originalname)
+    }
+});
+
 exports.uploadImage = async (req,res) => {
     const user_id = req.userId
-    // const user_id = 'doFavorAdminId'
-    const uploadImg = multer({storage: storageImg}).array('file');
+    const uploadImg = multer({storage: storageImg}).array('img');
 
     uploadImg( req, res, async (err) => {
         if (err) {
@@ -32,9 +45,45 @@ exports.uploadImage = async (req,res) => {
 
         const data = await Files.create(file_arr)
         const data_id = data.map(key => {
-            return key._id
+            return key.file_path
         });
 
         return res.status(200).json({result: 'OK', message: 'success upload image', data: data_id})
+    });
+};
+
+exports.uploadpdf = async (req,res) => {
+
+    const user_id = req.userId
+    const upload = multer({storage: storagePdf}).array('pdf')
+    upload( req, res, async (err) => {
+        if (err) {
+            return res.status(500).json(err)
+        }
+        const file_arr = []
+        for(let i = 0 ; i < req.files.length ; i++){
+            const file_data = {
+                file_name: req.files[i].originalname,
+                filename_extension: req.files[i].mimetype,
+                file_path: req.files[i].path,
+                file_author_id: user_id,
+            }
+            file_arr.push(file_data)
+        }
+
+        const data = await Files.create(file_arr)
+        const data_id = data.map(key => {
+            return key._id
+        });
+
+        const data_name = data.map(key => {
+            return key.file_name
+        });
+
+        const file_schema = [{
+            id: data_id,
+            file_name: data_name
+        }]
+        return res.status(200).json({result: 'OK', message: 'success upload file', data: file_schema})
     });
 };
--- src/routes/file.js
+++ src/routes/file.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const jwt = require('../jwt');
 const FileController = require('../controller/FileController');
 
+router.post('/uploadimg',jwt.verify, FileController.uploadImage);
+router.post('/uploadpdf',jwt.verify, FileController.uploadpdf);
 
 
 module.exports = router;
