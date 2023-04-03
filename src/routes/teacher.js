b43cafba189d66a380c08eec47161ac19dec10c6
Nattapon Suwanno <nattapon.tape@g.swu.ac.th>
2023-04-04 00:48:48 +0700

    Already

--- src/routes/teacher.js
+++ src/routes/teacher.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const jwt = require('../jwt');
 const TeacherController = require('../controller/TeacherController');
 
-router.post('/request-history',jwt.verify,TeacherController.reqHistory)
+router.get('/request-history',jwt.verify,TeacherController.reqHistory)
 router.get('/kpi-active',jwt.verify,TeacherController.getkpiactive)
 router.patch('/changepassword',jwt.verify,TeacherController.changepassword)
 router.post('/request',jwt.verify,TeacherController.createRequest)
